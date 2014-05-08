d8ce7a99261d82a7905fc85a1998aa287c110f77
myfluxi <linflux@arcor.de>
2014-05-08 08:53:00 -0700

    cpufreq: skateractive: Honor changed minimum frequency This saves a few cycles in the timer routine.
    Change-Id: I4c0e89094a00a0b63eae6320e6c3bdbb2d4c307
    Signed-off-by: myfluxi <linflux@arcor.de>
    Signed-off-by: Lonelyoneskatter <threesixoh.skater@yahoo.com>

--- drivers/cpufreq/cpufreq_skateractive.c
+++ drivers/cpufreq/cpufreq_skateractive.c
@@ -428,6 +428,7 @@ static void cpufreq_skateractive_timer(unsigned long data)
 	unsigned int loadadjfreq;
 	unsigned int index;
 	unsigned long flags;
+	unsigned int this_hispeed_freq;
 	struct cpufreq_govinfo int_info;
 
 	if (!down_read_trylock(&pcpu->enable_sem))
@@ -475,18 +476,19 @@ static void cpufreq_skateractive_timer(unsigned long data)
 
 	spin_lock_irqsave(&pcpu->target_freq_lock, flags);
 	cpu_load = loadadjfreq / pcpu->policy->cur;
+	this_hispeed_freq = max(tunables->hispeed_freq, pcpu->policy->min);
 
 	if (cpu_load >= tunables->go_hispeed_load) {
-		if (pcpu->policy->cur < tunables->hispeed_freq) {
-			new_freq = tunables->hispeed_freq;
+		if (pcpu->policy->cur < this_hispeed_freq) {
+			new_freq = this_hispeed_freq;
 		} else {
 			if (tunables->fastlane && cpu_load > tunables->fastlane_threshold)
 				new_freq = fastlane_freq(pcpu, cpu_load);
 			else
 			new_freq = choose_freq(pcpu, loadadjfreq);
 
-			if (new_freq < tunables->hispeed_freq)
-				new_freq = tunables->hispeed_freq;
+			if (new_freq < this_hispeed_freq)
+				new_freq = this_hispeed_freq;
 		}
 	} else {
 		if (tunables->fastlane && cpu_load > tunables->fastlane_threshold)
@@ -495,7 +497,7 @@ static void cpufreq_skateractive_timer(unsigned long data)
 		new_freq = choose_freq(pcpu, loadadjfreq);
 	}
 
-	if (pcpu->policy->cur >= tunables->hispeed_freq &&
+	if (pcpu->policy->cur >= this_hispeed_freq &&
 	    new_freq > pcpu->policy->cur &&
 	    now - pcpu->hispeed_validate_time <
 	    freq_to_above_hispeed_delay(tunables, pcpu->policy->cur)) {
@@ -545,12 +547,12 @@ static void cpufreq_skateractive_timer(unsigned long data)
 	/*
 	 * Update the timestamp for checking whether speed has been held at
 	 * or above the selected frequency for a minimum of min_sample_time,
-	 * if not boosted to hispeed_freq.  If boosted to hispeed_freq then we
+	 * if not boosted to this_hispeed_freq.  If boosted to this_hispeed_freq then we
 	 * allow the speed to drop as soon as the boostpulse duration expires
 	 * (or the indefinite boost is turned off).
 	 */
 
-	if (new_freq > tunables->hispeed_freq) {
+	if (new_freq > this_hispeed_freq) {
 		pcpu->floor_freq = new_freq;
 		pcpu->floor_validate_time = now;
 	}
