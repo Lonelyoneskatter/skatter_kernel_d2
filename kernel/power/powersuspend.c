85a18b268a4d51ae3eb49d6af172664636b2bde5
Lonelyoneskatter <threesixoh.skater@yahoo.com>
2017-06-23 23:44:08 -0700

    powersuspend: Use power efficient workqueues. Signed-off-by: Lonelyoneskatter <threesixoh.skater@yahoo.com>

--- kernel/power/powersuspend.c
+++ kernel/power/powersuspend.c
@@ -158,14 +158,16 @@ void set_power_suspend_state(int new_state)
 			#endif
 			state = new_state;
                         power_suspended = true;
-			queue_work(power_suspend_work_queue, &power_suspend_work);
+			queue_work(system_power_efficient_wq,
+				&power_suspend_work);
 		} else if (state == POWER_SUSPEND_ACTIVE && new_state == POWER_SUSPEND_INACTIVE) {
 			#ifdef CONFIG_POWERSUSPEND_DEBUG
 			pr_info("[POWERSUSPEND] state deactivated.\n");
 			#endif
 			state = new_state;
                         power_suspended = true;
-			queue_work(power_suspend_work_queue, &power_resume_work);
+			queue_work(system_power_efficient_wq,
+				&power_resume_work);
 		}
 		spin_unlock_irqrestore(&state_lock, irqflags);
 	#ifdef CONFIG_POWERSUSPEND_DEBUG
